64009972f37bd2a62a7a718f0c0c3043cac59b4c
Usuario <Usuario@DESKTOP-FT7LAG6>
2024-02-19 16:37:21 -0600

    Agregar archivos de proyecto.

---
+++ Exercise 2 U2/Program.cs
@@ -0,0 +1,22 @@
+﻿
+using Exercise_2_U2;
+
+namespace Exercise_2_U2
+{
+    class program
+    {
+
+        static void Main(string[] args)
+        {
+            formula_2 time = new formula_2();
+            time.v_cyclist(900, 90, 10);
+
+            Console.WriteLine("~The time a cyclist takes to reach the finish line~");
+            Console.WriteLine("Distance(m)= " + time.Distance + "(m)");
+            Console.WriteLine("velocity=" + time.Velocity + "(m/s)");
+            Console.WriteLine("time(s)= " + time.Time + "(s)");
+            Console.ReadLine();
+        }
+    }
+
+}
---
+++ Exercise 2 U2/formula 2.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise_2_U2
+{
+    public class formula_2
+    {
+        private int distance;
+        private int velocity;
+        private int time;
+
+        public int Distance { get => distance; set => distance = value; }
+        public int Velocity { get => velocity; set => velocity = value; }
+        public int Time { get => time; set => time = value; }
+
+        public void v_cyclist(int _distance, int _velocity, int _time)
+        {
+            this.distance = _distance;
+            this.velocity = _velocity;
+            this.time = _time;
+            
+        }
+    }
+}
